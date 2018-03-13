6cc4a822d6793155d8f20a19734cc3fbf1372889
anvais <antti.j.valtonen@alumni.aalto.fi>
2018-03-13 16:27:53 +0200

    Kontrolleria aloiteltu

--- src/main/java/foorumi/foorumi/Kontrolleri/Kontrolleri.java
+++ src/main/java/foorumi/foorumi/Kontrolleri/Kontrolleri.java
@@ -8,17 +8,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class Kontrolleri {
 
-    @RequestMapping("/")
+    @RequestMapping("/etusivu")
     public String Etusivu(Model model) {
         model.addAttribute("otsikko", "Etusivu");
         return "Etusivu";
     }
 
-//    @GetMapping("/Autohullut")
-//    public String Autohullut(Model model) {
-//        model.addAttribute("otsikko", "Autohullut");
-//        return "Autohullut";
-//    }
+    @RequestMapping("/autohullut")
+    public String Autohullut(Model model) {
+        model.addAttribute("otsikko", "Autohullut");
+        return "Autohullut";
+    }
+
+    @RequestMapping("/kalajutut")
+    public String Kalajutut(Model model) {
+        model.addAttribute("otsikko", "Kalajutut");
+        return "Kalajutut";
+    }
+
+    @RequestMapping("/leffalopinat")
+    public String Leffalopinat(Model model) {
+        model.addAttribute("otsikko", "Leffalopinat");
+        return "Leffalopinat";
+    }
+
+    @RequestMapping("/golfjuorut")
+    public String Golfjuorut(Model model) {
+        model.addAttribute("otsikko", "Golfjuorut");
+        return "Golfjuorut";
+    }
 
 
 
