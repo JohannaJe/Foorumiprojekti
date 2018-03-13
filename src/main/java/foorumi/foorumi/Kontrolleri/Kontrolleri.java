03be695833cd2fe0e3b2d26ea128b72e20e02090
anvais <antti.j.valtonen@alumni.aalto.fi>
2018-03-13 16:10:22 +0200

    Järjestetty kansioihin

--- src/main/java/foorumi/foorumi/Kontrolleri/Kontrolleri.java
+++ src/main/java/foorumi/foorumi/Kontrolleri/Kontrolleri.java
@@ -2,16 +2,24 @@ package foorumi.foorumi.Kontrolleri;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Kontrolleri {
 
-    @RequestMapping("/Etusivu")
-    public String indeksi(Model model) {
+    @RequestMapping("/")
+    public String Etusivu(Model model) {
         model.addAttribute("otsikko", "Etusivu");
         return "Etusivu";
     }
+
+//    @GetMapping("/Autohullut")
+//    public String Autohullut(Model model) {
+//        model.addAttribute("otsikko", "Autohullut");
+//        return "Autohullut";
+//    }
+
 
 
 
